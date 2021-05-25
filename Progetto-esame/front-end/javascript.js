c7c975d00da39232ec2048abcd9c94d39297b435
Leonardo200213 <50982851+Leonardo200213@users.noreply.github.com>
2021-05-25 15:19:00 +0200

    modificato js e collegamento con form

--- Progetto-esame/front-end/javascript.js
+++ Progetto-esame/front-end/javascript.js
@@ -1,3 +1,68 @@
+
+function inserisci(){
+	var giorno = $('#input_4_date').val();
+	var ora = $('#input_4_duration').val();
+    var zona = $('#input_4_timezone').val();
+
+
+
+
+	var lezione1 = $('input[name="tempera"]:checked').val();
+	var lezione2 = $('input[name="acquarello"]:checked').val();
+    var lezione3 = $('input[name="carboncino"]:checked').val();
+
+    var credito = $('input[name="credito"]:checked').val();
+    var paypal = $('input[name="paypal"]:checked').val();
+    
+	var prenotazione = {
+		"giorno": giorno,
+		"ora": ora,
+		"zona": zona
+    };
+    function richiesta(prenotazione);
+
+    var corso = {
+        "lezione1": lezione1,
+		"lezione2": lezione2,
+		"lezione3": lezione3, 
+        "credito": credito, 
+        "paypal": paypal
+    }
+    function richiesta(corso);
+    
+}
+    
+    function richiesta(prenotazione){
+$.ajax({
+    url: 'http://localhost/ProgettoEsame/Progetto-esame/back-end/utente.php',
+    method: 'POST',
+    data: JSON.stringify(prenotazione),
+    contenttype: 'json',
+    success: function (data, result, textStatus, xhr) {
+        //$("#printhere").html("stato: "+ xhr.status);
+        location = "riuscito.html";
+    },
+    error: function (xhr, textStatus, errorThrown) {
+        console.log(xhr.status);
+    }
+});
+    
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+/*
+Google Maps API
+
 function initMap() {
     var uluru = {
         lat: 43.84164642756516,
@@ -12,3 +77,4 @@ function initMap() {
         map: map,
     });
 }
+*/
